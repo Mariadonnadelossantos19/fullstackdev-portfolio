17d07c3e0246dde64dff7a8af4f81cf902ef082c
Ma. Donna Fidelino <102678677+Mariadonnadelossantos19@users.noreply.github.com>
2026-04-28 10:16:24 +0800

    adding the  profile  picture

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -1,6 +1,7 @@
 import { PortfolioPage } from '../components/PortfolioPage'
 import { SectionHeading } from '../components/SectionHeading'
 import { StatCard } from '../components/StatCard'
+import profileImage from '../assets/img/profile2.png'
 
 export function AboutPage({ pageIndex, active }) {
   return (
@@ -19,35 +20,41 @@ export function AboutPage({ pageIndex, active }) {
         <StatCard value="15+" label="PROJECTS SHIPPED" />
         <StatCard value="500+" label="TEST CASES WRITTEN" />
       </div>
-      <div className="about-text">
-        <span className="hl">/**</span>
-        <br />
-        &nbsp;* Hi, I&apos;m <span className="hl">Maria Donna</span> — a{' '}
-        <span className="hl2">Full Stack Developer</span> and
-        <br />
-        &nbsp;* <span className="hl3">QA Engineer</span> who lives at the intersection of
-        <br />
-        &nbsp;* building and breaking software.
-        <br />
-        &nbsp;*
-        <br />
-        &nbsp;* I architect scalable web applications from
-        <br />
-        &nbsp;* <span className="hl">database schemas</span> to <span className="hl2">pixel-perfect UIs</span>,
-        <br />
-        &nbsp;* while maintaining a rigorous testing mindset
-        <br />
-        &nbsp;* that catches bugs before users ever see them.
-        <br />
-        &nbsp;*
-        <br />
-        &nbsp;* I believe great software is written with empathy:
-        <br />
-        &nbsp;* empathy for the <span className="hl3">user</span>, the <span className="hl">team</span>, and the next
-        <br />
-        &nbsp;* developer who inherits your code.
-        <br />
-        &nbsp;* <span className="hl">*/</span>
+      <div className="about-layout">
+        <div className="about-text">
+          <span className="hl">/**</span>
+          <br />
+          &nbsp;* Hi, I&apos;m <span className="hl">Maria Donna</span> — a{' '}
+          <span className="hl2">Full Stack Developer</span> and
+          <br />
+          &nbsp;* <span className="hl3">QA Engineer</span> who lives at the intersection of
+          <br />
+          &nbsp;* building and breaking software.
+          <br />
+          &nbsp;*
+          <br />
+          &nbsp;* I architect scalable web applications from
+          <br />
+          &nbsp;* <span className="hl">database schemas</span> to <span className="hl2">pixel-perfect UIs</span>,
+          <br />
+          &nbsp;* while maintaining a rigorous testing mindset
+          <br />
+          &nbsp;* that catches bugs before users ever see them.
+          <br />
+          &nbsp;*
+          <br />
+          &nbsp;* I believe great software is written with empathy:
+          <br />
+          &nbsp;* empathy for the <span className="hl3">user</span>, the <span className="hl">team</span>, and the next
+          <br />
+          &nbsp;* developer who inherits your code.
+          <br />
+          &nbsp;* <span className="hl">*/</span>
+        </div>
+
+        <figure className="about-photo-card">
+          <img src={profileImage} alt="Maria Donna portrait" className="about-photo" loading="lazy" />
+        </figure>
       </div>
     </PortfolioPage>
   )
